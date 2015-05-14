ea5d8353e3081bb74431df8b75ac00e7ca1cafe5
Dkhanas <dkhanas@gmail.com>
2015-05-14 23:17:31 +0300

    add_html_and_flatpage

--- microsocial/microsocial/urls.py
+++ microsocial/microsocial/urls.py
@@ -1,8 +1,11 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.flatpages.views import flatpage
+
 from microsocial import views
 
 urlpatterns = [
     url(r'^$', views.main, name='main'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^about/$', flatpage, {'url':'/about/'},name='about_page'),
 ]
