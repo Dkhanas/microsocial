ef598fe3b543b1f8de320b4ea3f85216fac123e5
Dkhanas <dkhanas@gmail.com>
2015-05-14 17:57:51 +0300

    add_static

--- microsocial/microsocial/urls.py
+++ microsocial/microsocial/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from microsocial import views
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'microsocial.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+urlpatterns = [
+    url(r'^$', views.main, name='main'),
     url(r'^admin/', include(admin.site.urls)),
-)
+]
