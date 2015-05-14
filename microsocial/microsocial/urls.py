39fa9bf9ad20d64d82232b32225523457a22d1f2
Dkhanas <dkhanas@gmail.com>
2015-05-14 23:46:32 +0300

    add_all_flatpages

--- microsocial/microsocial/urls.py
+++ microsocial/microsocial/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     url(r'^$', views.main, name='main'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^about/$', flatpage, {'url':'/about/'},name='about_page'),
+    url(r'^help/$', flatpage, {'url':'/help/'},name='help_page'),
+    url(r'^advertising/$', flatpage, {'url':'/advertising/'}, name='advertising_page'),
+    url(r'^developers/$', flatpage, {'url':'/developers/'}, name='developers_page'),
+    url(r'^vacancies/$', flatpage, {'url':'/vacancies/'}, name='vacancies_page'),
+
 ]
