8ed4120198a40ae0d46457e5aad51c1da7099368
Dmytro Khanas <dkhanas@gmail.com>
2015-09-17 03:16:23 +0300

    added style for login

--- microsocial/views.py
+++ microsocial/views.py
@@ -1,8 +1,9 @@
 # coding=utf-8
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 
 
 @login_required
 def main(requests):
     return redirect('user_profile', user_id=requests.user.pk, permanent=False)
+    #return render(requests, "main.html")
