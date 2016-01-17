6957e0e6addebd2cb586022bed239a462c287e83
Dmytro <dkhanas@gmail.com>
2016-01-17 22:29:22 +0200

    fixed checked more one video on main page

--- microsocial/views.py
+++ microsocial/views.py
@@ -1,14 +1,19 @@
 # coding=utf-8
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
-
+from django.core.exceptions import MultipleObjectsReturned
 
 
 from videomain.models import VideoMainPage
 
 # @login_required
 def main(requests):
-    #TODO: 1:"Check last active"
-    # return redirect('user_profile', user_id=requests.user.pk, permanent=False)
-    video = VideoMainPage.objects.get(checked=True)
+    """
+    Take checked video. If checked more one then check last active
+    and show it.
+    """
+    try:
+        video = VideoMainPage.objects.get(checked=True)
+    except VideoMainPage.MultipleObjectsReturned:
+        video = VideoMainPage.objects.latest('last_active')
     return render(requests, 'main.html', {'video': video})
--- videomain/views.py
+++ videomain/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, render_to_response
 from django.template import Context
 from videomain.models import VideoMainPage
+from django.core.exceptions import MultipleObjectsReturned
 
 
 def video_for_main(request):
-    video = VideoMainPage.objects.get(checked=True)
+    try:
+        video = VideoMainPage.objects.get(checked=True)
+    except VideoMainPage.MultipleObjectsReturned:
+        video = VideoMainPage.objects.latest('last_active')
     return render(request, 'video_for_main/video_for_main.html', {'video': video})
 
