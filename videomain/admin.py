2350397a92ff5d49e9f6b3056f1bd77f7e4172f9
Dmytro <dkhanas@gmail.com>
2016-01-18 17:22:06 +0200

    changed video_admin

--- videomain/admin.py
+++ videomain/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from models import VideoMainPage
 
-admin.site.register(VideoMainPage)
+
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ('video_url', 'checked', 'last_active',)
+
+
+admin.site.register(VideoMainPage, VideoAdmin)
